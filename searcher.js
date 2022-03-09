98f052982e59fb7122152fb8ba70e6f89d9e7054
Jackson Holiday Wheeler <jhwheeler@gmail.com>
2022-03-09 20:41:58 +0800

    feat: save session; filter message results

--- searcher.js
+++ searcher.js
@@ -1,3 +1,4 @@
+const fs = require('fs').promises
 const { Api, TelegramClient } = require('telegram')
 const { StringSession } = require('telegram/sessions')
 const input = require('input')
@@ -6,62 +7,85 @@ require('dotenv').config();
 const {
   TELEGRAM_API_ID,
   TELEGRAM_API_HASH,
-  TELEGRAM_PHONE_NUMBER
+  TELEGRAM_PHONE_NUMBER,
 } = process.env
+
+const SESSION_KEY_FILE = './sessionKey.txt'
 
 const apiId = parseInt(TELEGRAM_API_ID, 10);
 
-const stringSession = new StringSession('');
+async function getSavedSessionKey () {
+  const data = await fs.readFile(SESSION_KEY_FILE, 'utf8')
+  return data
+}
 
-(async () => {
-  async function login () {
-    console.log('Loading interactive login');
-    await client.start({
-        phoneNumber: TELEGRAM_PHONE_NUMBER,
-        phoneCode: async () => await input.text('Code:'),
-        onError: (err) => console.log(err),
-    });
-    console.log('You are now connected');
+async function login (client) {
+  console.log('Loading interactive login');
 
-    // TODO: Save this string to avoid logging in again
-    console.log(client.session.save())
-  }
+  await client.start({
+    phoneNumber: TELEGRAM_PHONE_NUMBER,
+    phoneCode: async () => await input.text('Code:'),
+      onError: (err) => console.log(err),
+  });
+  console.log('You are now connected');
 
-  async function search () {
-    const query = await input.text('Query:')
+  // Save session key to avoid login every time
+  fs.writeFile(SESSION_KEY_FILE, client.session.save())
+}
 
-    const ONE_DAY = 86400; // seconds
-    const NUMBER_OF_DAYS = 7; // how many days to search back
+async function search (client) {
+  const query = await input.text('Query:')
 
-    const res = await client.invoke(new Api.messages.SearchGlobal({
-        flags: 1,
-        folderId: 0,
-        q: query,
-        filter: new Api.ChannelMessagesFilterEmpty(),
-        minDate: (Date.now()/1000) - (ONE_DAY * NUMBER_OF_DAYS),
-        offsetRate: 0,
-        offsetPeer: new Api.InputPeerEmpty(),
-        offsetId: 0,
-        limit: 10,
-    }));
+  const ONE_DAY = 86400; // seconds
+  const NUMBER_OF_DAYS = 7; // how many days to search back
 
-    console.log(`Search result for '${query}'`, res);
+  const res = await client.invoke(new Api.messages.SearchGlobal({
+    flags: 1,
+    folderId: 0,
+    q: query,
+    filter: new Api.ChannelMessagesFilterEmpty(),
+    minDate: (Date.now()/1000) - (ONE_DAY * NUMBER_OF_DAYS),
+    offsetRate: 0,
+    offsetPeer: new Api.InputPeerEmpty(),
+    offsetId: 0,
+    limit: 10,
+  }));
 
-    search();
-  }
+  const filteredResults = res.messages.reduce((filtered, messageObject) => {
+    const { scam, message, fromId, peerId } = messageObject
 
+    const userId = fromId ? fromId.userId : peerId.userId
+
+    if (scam) console.log('This message is scam', messageObject)
+      if (!scam) {
+        filtered.push({
+          message,
+          userId,
+        })
+      }
+
+      return filtered
+  }, [])
+
+  console.log(`Search result for '${query}'`, filteredResults);
+
+  search();
+}
+
+async function init () {
+  const sessionKey = await getSavedSessionKey()
 
   const client = new TelegramClient(
-    stringSession,
+    new StringSession(sessionKey),
     apiId,
     TELEGRAM_API_HASH,
     { connectionRetries: 5 }
   );
 
-    await login();
+  if (!client.session.save()) await login(client);
 
+  search(client);
+}
 
-    search();
-})();
-
+init()
 
