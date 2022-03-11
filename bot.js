92e52182bcaeda34d66f5a3a85f286cf55c0f3bb
Jackson Holiday Wheeler <jhwheeler@gmail.com>
2022-03-11 14:59:20 +0800

    feat: basic navigation from message to message

--- bot.js
+++ bot.js
@@ -1,32 +1,49 @@
 const Telegraf = require('telegraf').Telegraf
 const { Keyboard, Key } = require('telegram-keyboard')
-const flow = require('./bot-flow.json')
+const messages = require('./bot-messages.json')
 require('dotenv').config();
 
 const bot = new Telegraf(process.env.TELEGRAF_BOT_TOKEN);
 
-const backKeyboard = Keyboard.make(['Back'])
+const MESSAGE_NOT_FOUND = "Sorry, I don't understand your message. Please try something else."
 
-function main (ctx) {
+function generateKeyboard (node) {
+  const buttons = messages[node].buttons
+    .map(button => Key.callback(button.message, JSON.stringify(button.action)))
+
+  return Keyboard.make([buttons])
+}
+
+function init (ctx) {
   const welcomeMessage = 'Welcome to the Grow Ukraine Chat Bot'
-
-  const { root } = flow
-  const options = Object.keys(root.options)
-
-  const buttons = options.map(option => Key.callback(option, options[option]))
-  const keyboard = Keyboard.make([buttons])
+  const keyboard = generateKeyboard('root_message')
 
   ctx.reply(welcomeMessage, keyboard.inline())
 }
 
-bot.start(main)
+bot.start(init)
 
 bot.on('callback_query', (ctx) => {
-  const { data } = ctx.callbackQuery
+  const data = JSON.parse(ctx.callbackQuery.data)
   console.log('data', data)
-  return ctx.answerCbQuery(data.toString())
+
+  if (data.type === 'link') {
+    const destination = messages[data.destination]
+    if (!destination) {
+      ctx.reply(MESSAGE_NOT_FOUND)
+      return
+    }
+
+    const { message } = destination
+    if (!message) return
+
+    ctx.reply(message)
+  }
+
+  return ctx.answerCbQuery('Loading...')
 })
 
 bot.launch();
+
 console.log('Grow Ukraine bot is connected');
 
