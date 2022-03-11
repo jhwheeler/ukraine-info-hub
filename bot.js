66d63d1c4a380e1c07a96faec15d8e2091454631
Jackson Holiday Wheeler <jhwheeler@gmail.com>
2022-03-11 13:48:13 +0800

    feat: WIP parse bot flow

--- bot.js
+++ bot.js
@@ -1,10 +1,32 @@
 const Telegraf = require('telegraf').Telegraf
+const { Keyboard, Key } = require('telegram-keyboard')
+const flow = require('./bot-flow.json')
 require('dotenv').config();
-
-console.log('Ukraine Info Hub Bot is running');
 
 const bot = new Telegraf(process.env.TELEGRAF_BOT_TOKEN);
 
-bot.start(ctx => ctx.reply('Welcome to the Ukraine Info Hub. Our mission is to connect people in need with those who can help. This bot is currently under construction.'));
+const backKeyboard = Keyboard.make(['Back'])
+
+function main (ctx) {
+  const welcomeMessage = 'Welcome to the Grow Ukraine Chat Bot'
+
+  const { root } = flow
+  const options = Object.keys(root.options)
+
+  const buttons = options.map(option => Key.callback(option, options[option]))
+  const keyboard = Keyboard.make([buttons])
+
+  ctx.reply(welcomeMessage, keyboard.inline())
+}
+
+bot.start(main)
+
+bot.on('callback_query', (ctx) => {
+  const { data } = ctx.callbackQuery
+  console.log('data', data)
+  return ctx.answerCbQuery(data.toString())
+})
 
 bot.launch();
+console.log('Grow Ukraine bot is connected');
+
