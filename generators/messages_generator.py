537a7261809b0619ad16943378fa21bd7b529905
mxdr <max@playpilot.com>
2022-03-13 11:56:38 +0100

    feat: WIP implemnt flow into code

--- generators/messages_generator.py
+++ generators/messages_generator.py
@@ -1,13 +1,23 @@
 import json
+import pycountry
 
-from utils import message, action_link, action_input, button, links_to_check
+
+from utils import (
+    message,
+    action_link,
+    action_input,
+    button,
+    links_to_check,
+    get_country_importance,
+    fix_country_name,
+)
 
 
 messages = {
     'root': message(
         'What do you need?',
         buttons=[
-            button('Help', action_link('need_help_from_location')),
+            button('Help', action_link('i_am_now_in')),
             button('Donate', action_link('donate')),
             button('Main Info', action_link('main_info')),
             button('About Us', action_link('about_us')),
@@ -16,9 +26,78 @@ messages = {
     'donate': message('Thank you for helping. Here are charities collecting donations for people in need:'),
     'main_info': message('We have collected different groups and resources for people helping those in need:'),
     'about_us': message('Grow Ukraine is a group of volunteers working to solve the complex logistics of humanitarian aid delivery'),
-    'number_of_people_looking_for_shelter': message(
+    'i_am_now_in': message(
+        'I am now in...',
+        buttons=[
+            button('Country Ukraine', action_link('i_need'), context={'Country': 'Ukraine'}),
+            button('Other country', action_link('select_counry_currently_in')),
+        ],
+    ),
+    'select_counry_currently_in': message(
+        'Select a country',
+        buttons=[
+            button(fix_country_name(c.name), action_link('i_need'), context={'Country': fix_country_name(c.name)})
+            for c in sorted(pycountry.countries, key=lambda c: get_country_importance(c))
+        ],
+    ),
+    'i_need': message(
+        'I need',
+        buttons=[
+            button('Help', action_link('i_need_help'), context={'Need': 'Help'}),
+            button('Job', action_link('i_need_job'), context={'Need': 'Job'}),
+        ],
+    ),
+    'i_need_help': message(
+        'I need help with',
+        buttons=[
+            button('Shelter', action_link('i_need_shelter'), context={'NeedType': 'Shelter'}),
+            button('Medical support', action_link('i_need_medical_support'), context={'Need': 'MedicalSupport'}),
+            button('Material support', action_link('i_need_material_support'), context={'Need': 'Material'}),
+            button('Jewish organizations', action_link('i_need_jewish_organization'), context={'Need': 'Jews'}),
+            button('Psychological support', action_link('i_need_psychological'), context={'Need': 'Psychological'}),
+            button('Lawyers', action_link('i_need_lawyers'), context={'Need': 'Lawyer'}),
+        ],
+    ),
+    'i_need_shelter': message(
         'How many people are looking for shelter?',
-        action=action_input('number', 'how_many_days_need_shelter'),
+        action=action_input('number', 'how_many_people_need_shelter'),
+    ),
+    'how_many_people_need_shelter': message(
+        'How many people are looking for shelter?',
+        action=action_input('number', 'data_processing_agreement'),
+    ),
+    'i_need_medical_support': message(
+        'What kind of medical support do you need?',
+        action=action_input('text', 'data_processing_agreement'),
+    ),
+    'i_need_material_support': message(
+        'What kind of material support do you need?',
+        action=action_input('text', 'data_processing_agreement'),
+    ),
+    'i_need_jewish_organization': message(
+        '___JEWISH INFO___',
+        action=action_input('text', 'data_processing_agreement'),
+    ),
+    'i_need_psychological': message(
+        'Check out the following information: ___PSYCHOLOGICAL INFO___',
+        action=action_input('text', 'root'),
+    ),
+    'i_need_lawyers': message(
+        'Check out the following information: ___LAWYER INFO___',
+        action=action_input('text', 'root'),
+    ),
+    'data_processing_agreement': message(
+        'Let\'s proceed.\nDo you give your consent tot the processing of your personal data (___agreement url___)?',
+        buttons=[
+            button('No', action_link('______i_do_not_consent')),
+            button('Share your telegram contact', action_link('______')),
+        ],
+    ),
+    'i_need_job': message(
+        '___ JOB INFO ___',
+        buttons=[
+            button('Back to main menu', action_link('root')),
+        ],
     ),
 }
 
@@ -30,10 +109,12 @@ for link in links_to_check:
         print(f'Referenced link "{link}" is not in messages')
         do_exit = True
 
-if do_exit:
-    print('\n------- ( ! ) ------- \nYou have links to messages which don\'t exist (see above). Please resolve this and try again. No output file generated.\n------- ( ! ) -------')
-    exit()
+
+# TODO: uncomment
+# if do_exit:
+#     print('\n------- ( ! ) ------- \nYou have links to messages which don\'t exist (see above). Please resolve this and try again. No output file generated.\n------- ( ! ) -------')
+#     exit()
 
 
-with open('output.json', 'w') as f:
+with open('messages.json', 'w') as f:
     f.write(json.dumps(messages, indent=2))
--- generators/utils.py
+++ generators/utils.py
@@ -1,3 +1,6 @@
+import pycountry_convert
+
+
 links_to_check = []
 
 
@@ -29,6 +32,10 @@ def validate_buttons(buttons):
     return [validate_button(button) for button in buttons]
 
 
+def validate_context(context):
+    return context
+
+
 def action_link(destination):
     global links_to_check
     links_to_check.append(destination)
@@ -50,10 +57,11 @@ def action_input(input_type, destination):
     }
 
 
-def button(message, action):
+def button(message, action, context=None):
     return {
         'message': message,
         'action': action,
+        **({'context': validate_context(context)} if context else {}),
     }
 
 
@@ -63,3 +71,36 @@ def message(message, buttons=None, action=None):
         **({'buttons': validate_buttons(buttons)} if buttons else {}),
         **({'action': validate_action(action)} if action else {}),
     }
+
+
+def get_country_importance(country):
+    if country.alpha_2 == 'PL':
+        return 1
+
+    if country.alpha_2 == 'HU':
+        return 2
+
+    if country.alpha_2 in ['PL', 'SK', 'RO', 'MD', 'GE']:
+        return 3
+
+    if country.alpha_2 in ['EE', 'LT', 'LV', 'DE', 'CZ', 'SL', 'CR', 'RS', 'MC', 'BG']:
+        return 4
+
+    try:
+        continent = pycountry_convert.country_alpha2_to_continent_code(country.alpha_2)
+    except Exception:
+        return 9
+
+    if continent == 'EU':
+        return 5
+
+    return 9
+
+
+def fix_country_name(country_name):
+    if country_name == 'Taiwan, Province of China':
+        return 'Taiwan'
+
+    # TODO: remove `, United Republic of` from Tanzania etc.
+
+    return country_name
